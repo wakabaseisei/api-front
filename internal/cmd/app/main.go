1909462fa1c19d35ba0a6d7bd436b3ff022fc2e3
wakabaseisei <wakabaseisei@gmail.com>
2025-04-13 12:10:38 +0900

    mod

--- internal/cmd/app/main.go
+++ internal/cmd/app/main.go
@@ -93,7 +93,7 @@ func withCORS(h http.Handler) http.Handler {
 	middleware := cors.New(cors.Options{
 		// TODO:
 		AllowedOrigins: []string{"*"},
-		AllowedMethods: append(connctcors.AllowedMethods(), "OPTIONS"),
+		AllowedMethods: connctcors.AllowedMethods(),
 		AllowedHeaders: connctcors.AllowedHeaders(),
 		ExposedHeaders: connctcors.ExposedHeaders(),
 	})
