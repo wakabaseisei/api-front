ef54023e2d781065502cb9a1fa5bd6f36ab981e0
wakabaseisei <wakabaseisei@gmail.com>
2025-05-01 15:43:17 +0900

    fix grpc client request param

--- internal/driver/client/user_client.go
+++ internal/driver/client/user_client.go
@@ -6,9 +6,12 @@ import (
 	"net/http"
 
 	"buf.build/gen/go/wakabaseisei/ms-protobuf/connectrpc/go/ms/user/v1/userv1connect"
+	userv1 "buf.build/gen/go/wakabaseisei/ms-protobuf/protocolbuffers/go/ms/user/v1"
+	"connectrpc.com/connect"
 	"github.com/wakabaseisei/api-front/internal/domain"
 	"github.com/wakabaseisei/api-front/internal/domain/service"
 	"github.com/wakabaseisei/api-front/internal/driver/client/converter"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type userService struct {
@@ -33,7 +36,17 @@ func (s *userService) CreateUser(
 	*domain.User,
 	error,
 ) {
-	res, serr := s.client.CreateUser(ctx, nil)
+	user := userv1.User{
+		UserId:    cmd.ID,
+		Name:      cmd.Name,
+		CreatedAt: timestamppb.New(cmd.CreatedAt),
+	}
+
+	res, serr := s.client.CreateUser(ctx, connect.NewRequest(
+		&userv1.CreateUserRequest{
+			User: &user,
+		},
+	))
 	if serr != nil {
 		return nil, fmt.Errorf("CreateUser: %w", serr)
 	}
